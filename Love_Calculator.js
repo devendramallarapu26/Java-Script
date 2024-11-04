676dc303eea5e5f96c6623be2395eceec0f77bcc
Devendra <devendramallarapu8@gmail.com>
2024-11-04 09:47:13 +0530

    Create Love_Calculator.js

---
+++ Love_Calculator.js
@@ -0,0 +1,6 @@
+var name1 = prompt("whats your name");
+var name2 = prompt("whats you partner name");
+var per = Math.random();
+per = Math.floor(per*100)+1;
+console.log(name1+" Love "+name2)
+console.log("Love per is:"+per)
