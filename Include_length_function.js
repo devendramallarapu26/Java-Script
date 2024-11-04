b9267d97e9b019b219e7192635e7992fc1672653
Devendra <devendramallarapu8@gmail.com>
2024-11-04 13:01:34 +0530

    Create Include_length_function.js

---
+++ Include_length_function.js
@@ -0,0 +1,4 @@
+var names=["devendra","veerendra","vishnu","Balaiah"];
+console.log(names.length)
+console.log(names.include("devendra"))
+// its devendra is there in array its return the true ar else false
