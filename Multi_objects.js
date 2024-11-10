9f26398e38c621ac6a39097351e6554fe15d219d
Devendra <devendramallarapu8@gmail.com>
2024-11-10 07:57:10 +0530

    Create Multi_objects.js

---
+++ Multi_objects.js
@@ -0,0 +1,9 @@
+function Worker(name,age,lang) {
+    this.name = name;
+    this.age = age;
+    this.lang = lang;
+}
+var worker1 = new Worker("Devendra",20,["telugu","english"])
+var worker2 = new Worker("Veerendra",19,["telugu","Hindi"])
+var worker3 = new Worker("Vishnu",20,"telugu")
+var worker4 = new Worker("Dev",22,["telugu","english"])
